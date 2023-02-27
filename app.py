6a263d8b9bfcea1b3a96e699c22665b4525a918b
Hyewon Kim <hkim444@my.bcit.ca>
2023-02-27 08:42:13 -0800

    cleaned up code

--- app.py
+++ app.py
@@ -37,71 +37,12 @@ def add_student():
         school.save()
         return render_template("list.html", school=school)
 
-    # data = request.json
-    # if "name" in data.keys() and "student_id" in data.keys():
-    #     name = data["name"]
-    #     student_id = data["student_id"]
-
-    #     school_obj = School("./models/bcit.json")
-    #     try:
-    #         School.add_student(school_obj, name, student_id)
-    #     except ValueError:
-    #         return "invalid data",400
-    #     except TypeError:
-    #         return "student id already exists",409
-
-            
-    #     School.save(school_obj)
-    #     return render_template("list.html", school=school_obj)
-    # else:
-    #     return "only name provided",400
-    
-    # return "working"
-
-# @app.route("/student/<str:student_id>/grades/add", methods=["POST"])
-# def add_grade(student_id): 
-#     data = request.json 
-#     grade = data["grade"]
-#     school = School("./models/bcit.json")
-#     student = School.get_student(student_id)
- 
-#     try:
-#         School.get_student(student_id)
-#     except:
-#         return 404
-    
-#     try:
-#         student.add_grade(grade) 
-#     except ValueError:
-#         return 400
-    
-#     school.save()
-#     return render_template("student.html", student=student), 200
-        
-
-    # if student_id not in School.get_student.keys():
-    #     return 404 
-    
-    # elif "grade" not in data.keys():
-    #     return 400
-
-    # if data["grade"].isnumeric() & 0 <= int(data["grade"]) <= 100: 
-    #     student.add_grade(data["grade"])
-    #     school.save()
-    #     return render_template("student.html", student=student), 200  
-    # else:
-    #     return 400 
-
-
-
 @app.route("/student/<string:student_id>/grades/add", methods=["POST"])
 def add_a_grade(student_id):
     data = request.json
     school = School("./models/bcit.json")
     student = school.get_student(student_id)
     grade = data["grade"]
-    
-    # student = School.get_student(student_id)
 
     if "grade" not in data.keys():
         return 400 
